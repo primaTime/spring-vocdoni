1f13603fb5deeaed4465bcc5316429d9de9b0312
Erik Bystroň <erik.bystron@icloud.com>
2023-08-02 17:02:30 +0200

    Updated web3j

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-	java
-	`maven-publish`
 	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.0"
-	id("org.graalvm.buildtools.native") version "0.9.20"
+	`maven-publish`
+	`java-library`
 }
 
 group = "dev.trustproject"
@@ -48,7 +47,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.google.protobuf:protobuf-java:3.21.12")
-	implementation("org.web3j:crypto:5.0.0")
+	implementation("org.web3j:crypto:4.10.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
